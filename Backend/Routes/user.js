12ba65894a74cae59b5ba186d7a75d1c41acc6de
HarshP-ITL <harsh.p@intutechlab.com>
2024-07-19 19:06:35 +0530

    created models

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 const userController = new (require('../Controllers/user.controller'));
 
 // User signup
-router.route('/user/sign_up').post(userController.signUp)
+router.route('/user/sign_up').get(userController.signUp)
+
+module.exports = router;
